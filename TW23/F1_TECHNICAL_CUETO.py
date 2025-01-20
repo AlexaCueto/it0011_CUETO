929ebddc2fa3a42665a1366a49f23ac79bcf43b3
Alexa Cueto <167626858+AlexaCueto@users.noreply.github.com>
2025-01-20 10:44:16 +0800

    F1TECHNICAL, USER INPUT - 20/01/2025

---
+++ TW23/F1_TECHNICAL_CUETO.py
@@ -0,0 +1,16 @@
+firstName = input("Enter first name: ") #this is how to ask user input
+lastName = input("Enter last name: ") 
+middleName = input("Enter middle name: ") 
+birthDay = input("Enter birthday (MM/DD/YY): ") 
+gender = input("Enter gender(Male/Female): ") 
+address = input("Enter address: ") 
+course = input("Enter course: ") 
+yearLevel = int(input("Enter year level: "))
+
+
+print("Name: ", firstName, " ", middleName, " ", lastName )
+print("Birthday: ", birthDay)
+print("Gender: ", gender)
+print("Address: " , address)
+print("Course: ", course)
+print("Year Level: ", yearLevel)
--- TW23/act1.py
+++
@@ -1,5 +0,0 @@
-name = "Alexa Joyce" #this is how to declare variables
-print("My name is: ", name )
-
-name = input("Enter name: ") #this is how to ask user input
-print("My name is: ", name )
