1f7347e321ddcefdd5f9d8d3f192e9ed002ab940
Alexa Cueto <167626858+AlexaCueto@users.noreply.github.com>
2025-01-31 20:52:25 +0800

    modified 3 b

--- TA1/TA1_4_CUETO.py
+++ TA1/TA1_4_CUETO.py
@@ -8,7 +8,7 @@ print("(B) WHILE NESTED LOOP")
 
 i = 1
 while i <= 7: #prints the number of rows
-    if i % 2 != 0:  #odd numbers
+    if i % 2 != 0 or i == 6:  #numbers
         j = 1
         while j <= i:
             print(i, end="")
