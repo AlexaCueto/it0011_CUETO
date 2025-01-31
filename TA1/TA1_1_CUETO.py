05e3c318f5b3c06d3bb4119bfc1d3a415ce0e572
Alexa Cueto <167626858+AlexaCueto@users.noreply.github.com>
2025-01-31 20:06:37 +0800

    DONE NUMBER 1 31/01/25

---
+++ TA1/TA1_1_CUETO.py
@@ -0,0 +1,29 @@
+stringWord = input("Enter a word: ")
+i = stringWord
+
+print("The word you entered is: ", i)
+
+vowels = "aeiou" 
+consonants = "bcdfghjklmnpqrstvwxyz"
+whitespace = " "
+
+countVowels = 0
+countConsonants = 0
+countWhitespace = 0
+countDigits = 0
+countSymbols = 0
+
+for i in range(0, len(stringWord)):
+    if stringWord.lower()[i] in vowels:
+        countVowels = countVowels + 1
+    elif stringWord.lower()[i] in consonants:
+        countConsonants = countConsonants + 1
+    elif stringWord.lower()[i] in whitespace:
+        countWhitespace = countWhitespace + 1
+    else:
+        countSymbols = countSymbols + 1
+
+print("Number of Vowels: ", countVowels)
+print("Number of Consonants: ", countConsonants)
+print("Number of Whitespace: ", countWhitespace)
+print("Number of Symbols: ", countSymbols)
