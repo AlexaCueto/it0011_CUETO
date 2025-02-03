a5e97dbfe1fd9c52ddcf8904e27b06cd596c6bca
Alexa Cueto <167626858+AlexaCueto@users.noreply.github.com>
2025-02-03 10:31:58 +0800

    done

--- TFA1/3_1.py
+++ TFA1/3_1.py
@@ -6,7 +6,7 @@ fName = input("Enter your first name: ")
 lName = input("Enter your last name: ")
 age = int(input("Enter your age: "))
 
-slicedFName = fName[:3]
+slicedFName = fName[0:3]
 formattedString = f"Hello, {slicedFName}! Welcome. You are {age} years old. "
 
 print("Full Name: ", fName + " " + lName)
